3b0fe3bd22fffaa770333cd624963570f99f7d37
Paul Taele <ptaele@gmail.com>
2020-05-22 02:10:30 -0500

    Added ShortStraw algorithm.

--- basic-sketch-algorithms/js/viewCanvas.js
+++ basic-sketch-algorithms/js/viewCanvas.js
@@ -6,37 +6,38 @@ globals.runAlgorithm = (sketch, algorithmValue) => {
     drawStrokes(sketch, COLOR_GREY);
 
     // run algorithms
-    if      (algorithmValue === "endpoints")        { runEndpoints(sketch); }
-    else if (algorithmValue === "boundingBox")      { runBoundingBox(sketch); }
-    else if (algorithmValue === "boundingBoxes")    { runBoundingBoxes(sketch); }
-    else if (algorithmValue === "centerMidpoint")   { runCenterMidpoint(sketch); }
-    else if (algorithmValue === "centerCentroid")   { runCenterCentroid(sketch); }
+    if      (algorithmValue === "endpoints")            { runEndpoints(sketch); }
+    else if (algorithmValue === "boundingBox")          { runBoundingBox(sketch); }
+    else if (algorithmValue === "boundingBoxes")        { runBoundingBoxes(sketch); }
+    else if (algorithmValue === "centerMidpoint")       { runCenterMidpoint(sketch); }
+    else if (algorithmValue === "centerCentroid")       { runCenterCentroid(sketch); }
     else if (algorithmValue === "translate")        {
         
         let x = Number.parseInt(document.getElementById("translateX").value);
         let y = Number.parseInt(document.getElementById("translateY").value);
         runTranslate(sketch, x, y);
     }
-    else if (algorithmValue === "resampleCount")    {
+    else if (algorithmValue === "resampleCount") {
 
         let count = Number.parseInt(document.getElementById("resampleCount").value);
         runResampleCount(sketch, count);
     }
-    else if (algorithmValue === "resampleDistance")    {
+    else if (algorithmValue === "resampleDistance") {
 
         let pixels = Number.parseInt(document.getElementById("resampleDistance").value);
         runResampleDistance(sketch, pixels);
     }
-    else if (algorithmValue === "scaleProportional")    {
+    else if (algorithmValue === "scaleProportional") {
 
         let pixels = Number.parseInt(document.getElementById("scaleProportional").value);
         runScaleProportional(sketch, pixels);
     }
-    else if (algorithmValue === "scaleSquare")    {
+    else if (algorithmValue === "scaleSquare") {
 
         let pixels = Number.parseInt(document.getElementById("scaleSquare").value);
         runScaleSquare(sketch, pixels);
     }
+    else if (algorithmValue === "cornersShortStraw")    { runCornersShortStraw(sketch); }
 }
 
 function runEndpoints(sketch) {
@@ -190,6 +191,33 @@ function runScaleSquare(sketch, pixels) {
     console.log(newSketch);
 }
 
+function runCornersShortStraw(sketch) {
+
+    // get the resampled sketch and its corner indices
+    var resampledSketch = SketchRecTools.resampleByDistance(sketch);
+    var sketchCornerIndices = ShortStraw.run(resampledSketch);
+    
+    // debug
+    console.log(sketchCornerIndices);
+
+    // gather the corners from their indices
+    var corners = [];
+    for (let i = 0; i < resampledSketch.strokes.length; i++) {
+        let resampledPoints = resampledSketch.strokes[i].points;
+        let strokeCornerIndices = sketchCornerIndices[i];
+        for (let j = 0; j < strokeCornerIndices.length; j++) {
+            corners.push(resampledPoints[strokeCornerIndices[j]]);
+        }
+    }
+
+    // draw corners
+    for (let corner of corners) {
+
+        let dot = new Path.Circle(new Point(corner.x, corner.y), DOT_SIZE);
+        dot.fillColor = 'red';
+    }
+}
+
 
 
 function drawStrokes(sketch, color) {
@@ -241,6 +269,8 @@ const DOT_SIZE = 8;
 const COLOR_GREY    = "#808080";
 const COLOR_BLACK   = "#000000";
 const COLOR_RED     = "#ff0000";
+
+
 
 
 
@@ -589,4 +619,183 @@ let SketchRecTools = {
   
       return newSketch;
     },
+}; // end
+
+
+let ShortStraw = {
+    run: function(sketch) {
+        // return no corners for an empty sketch
+        if (sketch.strokes.length === 0) { return []; }
+  
+        let strokes = sketch.strokes;
+        let sketchCorners = [];
+        for (let i = 0; i < strokes.length; i++) {
+            let points = strokes[i].points;
+            let strokeCorners = this.findShortStrawCorners(points);
+            sketchCorners.push(strokeCorners);
+        }
+        return sketchCorners;
+    },
+  
+    findShortStrawCorners: function(points) {
+  
+        let corners = [];
+  
+        // ----- start algorithm -----
+        // add the first point index
+        corners.push(0);
+  
+        // handle singleton-stroke case
+        if (points.length === 1) { return corners; }
+  
+        // create the straws and straw range
+        let straws = [];
+  
+        // collect the straw distances
+        for (let i = this.W; i < points.length - this.W; i++) {
+            let straw0 = points[i - this.W];
+            let strawN = points[i + this.W];
+            let strawDistance = SketchRecTools.calculateDistance(straw0.x, straw0.y, strawN.x, strawN.y);
+            straws.push(strawDistance);
+        }
+  
+        // calculate the pseudo-median
+        let t = ShortStraw.calculateMedian(straws) * 0.95;
+  
+        // iterate through each straw distance
+        for (let i = 0; i < straws.length; i++) {
+  
+            // case: the current straw distance is less than the pseudo-median
+            if (straws[i] < t) {
+    
+                // initialize the local min and min index's starting values
+                let localMin = Number.MAX_SAFE_INTEGER;
+                let localMinIndex = i;
+  
+                // iterate through the local straw distance cluster
+                while (i < straws.length && straws[i] < t) {
+  
+                    // local min (i.e., corner index) candidate found
+                    if (straws[i] < localMin) {
+                        localMin = straws[i];
+                        localMinIndex = i;
+                    }
+    
+                    // iterate through the local cluster
+                    // note: need to iterate through i in inner loop to skip local cluster in outer loop
+                    i = i + 1;
+                }
+  
+                // add the corner index of local cluster to the corner indices array
+                // note: need to add W to offset between the straw indices and point indices
+                corners.push(localMinIndex + this.W);
+            }
+        }
+  
+        // add the last point index
+        corners.push(points.length - 1);
+  
+        corners = this.postProcessCorners(points, corners, straws);
+        return corners;
+    },
+  
+    postProcessCorners: function(points, corners, straws) {
+        // ----- start corner post-processing check #1 -----
+        let advance = false;
+        while (!advance) {
+            advance = true;
+  
+            // iterate through the corner indices
+            for (let i = 1; i < corners.length; i++) {
+                // get the previous and current corner indices
+                let c1 = corners[i - 1];
+                let c2 = corners[i];
+  
+                // check if line is formed between previous and current corner indices
+                let isLine = this.isLine(points, c1, c2);
+                if (!this.isLine(points, c1, c2)) {
+  
+                // get the candidate halfway corner
+                // offset it by W due to straw indices and points indices mis-match
+                var newCorner = this.halfwayCorner(straws, c1, c2);
+                newCorner = newCorner + this.W;
+  
+                // skip adding new corner, since it already exists
+                // can happen during an overzealous halfway corner calculation
+                if (newCorner === c2) {
+                    continue;
+                }
+  
+                corners.splice(i, 0, newCorner);
+                advance = false;
+            }
+        }
+  
+        // emergency stop
+        if (corners.length > 15) { console.log("WARNING: Infinite Loop"); break; }
+        }
+        // ----- end corner post-processing check #1 -----
+  
+        // ----- start corner post-processing check #2 -----
+        for (let i = 1; i < corners.length - 1; i++) {
+            let c1 = corners[i - 1];
+            let c2 = corners[i + 1];
+  
+            let isLine = this.isLine(points, c1, c2);
+            if (isLine) {
+                corners.splice(i, 1);
+                i = i - 1;
+            }
+        }
+  
+        // ----- end corner post-processing check #2 -----
+        return corners;
+    },
+  
+    isLine: function(points, a, b) {
+  
+        let subset = points.slice(a, b + 1);
+    
+        let threshold = 0.95;
+        let startPoint = points[a];
+        let endPoint = points[b];
+    
+        let ax = startPoint.x;
+        let ay = startPoint.y;
+        let bx = endPoint.x;
+        let by = endPoint.y;
+        let distance = SketchRecTools.calculateDistance(ax, ay, bx, by);
+        let pathDistance = SketchRecTools.calculatePathLength({ strokes: [ {points: subset} ] });
+        return distance / pathDistance > threshold;
+    },
+  
+    halfwayCorner: function(straws, a, b) {
+  
+        let quarter = Math.floor((b - a) / 4);
+        let minValue = Number.MAX_SAFE_INTEGER;
+        let minIndex = a + quarter;
+  
+        for (let i = a + quarter; i < b - quarter; i++) {
+            if (straws[i] < minValue) {
+                minValue = straws[i];
+                minIndex = i;
+            }
+        }
+  
+        return minIndex;
+    },
+  
+    calculateMedian: function(inputs) {
+        var values = [];
+        for (let i = 0; i < inputs.length; i++) {
+            values.push(inputs[i]);
+        }
+    
+        values.sort( function(a,b) {return a - b;} );
+        var half = Math.floor(values.length/2);
+        if (values.length % 2)  { return values[half]; }
+        else                    { return (values[half-1] + values[half]) / 2.0; }
+    },
+    
+    W: 3,
 }; // end
